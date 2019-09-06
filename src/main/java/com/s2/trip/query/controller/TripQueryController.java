88743583ff17a56f3f9163fd4e5e1567a5d91b25
Suresh Ramasamy <suresh.ramasamy2@anthem.com>
2019-09-06 20:48:06 +0530

    Added user details as header

--- src/main/java/com/s2/trip/query/controller/TripQueryController.java
+++ src/main/java/com/s2/trip/query/controller/TripQueryController.java
@@ -1,7 +1,7 @@
 package com.s2.trip.query.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,8 +10,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class TripQueryController {
 
 	@RequestMapping(value = "trip-query/summary", method = RequestMethod.GET)
-    @ResponseBody
-    public String currentUserName(Authentication authentication) {
-        return authentication.getName();
-    }
+	@ResponseBody
+	public String currentUserName(@RequestHeader(value = "username", required = true) String username,
+			@RequestHeader(value = "roles", required = true) String roles) {
+		System.out.println(username);
+		System.out.println(roles);
+		return "test";
+	}
 }
